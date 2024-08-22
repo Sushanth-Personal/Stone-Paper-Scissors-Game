70d42a9bdd7e40015815ca6d1d59bc9a90235425
Sushanth-Personal <168424396+Sushanth-Personal@users.noreply.github.com>
2024-08-22 19:24:24 +0530

    Halo animation done and will shift between pc and user based on the winner

--- script.js
+++ script.js
@@ -8,7 +8,11 @@ let signLeft = document.querySelector(".hand-sign-1");
 let signRight = document.querySelector(".hand-sign-2");
 let signBottom = document.querySelector(".hand-sign-3");
 
-let url=["images/scissor.png","images/paper.png","images/stone.png"]
+let url = ["images/scissor.png", "images/paper.png", "images/stone.png"];
+let yourChoiceStyle = ["hand-sign-1", "hand-sign-2", "hand-sign-3"];
+let pcChoice, yourChoice, winner;
+
+//Rules Section
 
 closeButton.addEventListener("click", () => {
   rules.style.transformOrigin = "top right";
@@ -23,8 +27,6 @@ rulesButton.addEventListener("click", () => {
 });
 
 function clickAnimation(sign) {
-  
-
   document.querySelector(".line1").style.display = "none";
   document.querySelector(".line2").style.display = "none";
   document.querySelector(".line3").style.display = "none";
@@ -40,12 +42,14 @@ function clickAnimation(sign) {
   signRight.style.transform = "translateX(-128px)";
 
   if (sign == "scissor") {
+    yourChoice = 0;
     signBottom.style.borderColor = "#0074b6";
     document.querySelector(".hand-sign-3 img").src = "images/scissor.png";
     document.querySelector(".hand-sign-1 img").src = "";
     document.querySelector(".hand-sign-2 img").src = "";
   }
   if (sign == "paper") {
+    yourChoice = 1;
     signBottom.style.borderColor = "#bd00ff";
     document.querySelector(".hand-sign-3 img").src = "images/paper.png";
     document.querySelector(".hand-sign-1 img").src = "";
@@ -53,47 +57,51 @@ function clickAnimation(sign) {
   }
 
   if (sign == "stone") {
+    yourChoice = 2;
     signBottom.style.borderColor = "#ffa943";
     document.querySelector(".hand-sign-3 img").src = "images/stone.png";
     document.querySelector(".hand-sign-1 img").src = "";
     document.querySelector(".hand-sign-2 img").src = "";
   }
 
-
   setTimeout(() => {
     signLeft.style.transition = "0s";
     signLeft.style.transform = "scale(0)";
-    signBottom.style.transform = "translateX(-250px) translateY(-135px)";
-    signRight.style.transform = "translateX(150px) translateY(100px)";
+    signBottom.style.transform = "translateX(-21.5vw) translateY(-135px)";
+    signRight.style.transform = "translateX(16vw) translateY(100px)";
     signRight.style.width = "140px";
     signRight.style.height = "140px";
     signBottom.style.width = "140px";
     signBottom.style.height = "140px";
 
-  
+    signBottom.addEventListener("transitionend", () => {
+      signLeft.style.transition = "0s";
+      signRight.style.transition = "0s";
+      signBottom.style.transition = "0s";
+    });
   }, 500); // Adjust the delay to match your transition duration (0.5s)
 
-  setTimeout(()=>{
-    document.querySelector('.halo').style.display='flex';
-  },1100);
-  
+  setTimeout(() => {
+    document.querySelector(".halo").style.display = "flex";
+    document.querySelector(".you-picked").style.transform =
+      "translateX(-20vw) translateY(270px)";
+    document.querySelector(".you-picked").style.color = "white";
+  }, 1100);
 
+  signLeft.removeEventListener("click", leftHandler);
+  signRight.removeEventListener("click", rightHandler);
+  signBottom.removeEventListener("click", bottomHandler);
 
-signLeft.removeEventListener("click",leftHandler);
-signRight.removeEventListener("click",rightHandler);
-signBottom.removeEventListener("click",bottomHandler);
-
-pcSelectionAnimation();
-
+  pcSelectionAnimation();
 }
 
-function leftHandler(){
+function leftHandler() {
   clickAnimation("scissor");
 }
-function rightHandler(){
+function rightHandler() {
   clickAnimation("paper");
 }
-function bottomHandler(){
+function bottomHandler() {
   clickAnimation("stone");
 }
 
@@ -102,47 +110,193 @@ signRight.addEventListener("click", rightHandler);
 signBottom.addEventListener("click", bottomHandler);
 
 function pcSelectionAnimation() {
-  let img=document.querySelector('.hand-sign-2 img');
-  let text=document.querySelector('.loading-text');
+  let img = document.querySelector(".hand-sign-2 img");
 
-  setTimeout(()=>{
-    
-    img.style.width='100px';
-    img.style.height='100px';
-    setInterval(() => {
-      text.style.color='white';
-    }, 150);
-    img.src="images/loading.png";
-    
-    img.style.transition='transform 3s linear';
-  img.style.transform='rotate(1800deg)';
- 
-  },1200);
-  img.addEventListener('transitionend', () => {
+  setTimeout(() => {
+    img.style.width = "100px";
+    img.style.height = "100px";
+
+    img.src = "images/loading.png";
+
+    img.style.transition = "transform 3s linear";
+    img.style.transform = "rotate(1800deg)";
+  }, 1200);
+  img.addEventListener("transitionend", () => {
     pcSelection();
+    removeFirstScreen();
   });
 }
 
-function pcSelection(){
-  let imgChoice=getRandomNumber();
-  let img=document.querySelector('.hand-sign-2 img');
-  img.src=`${url[imgChoice]}`;
-  if(imgChoice==0){
-    img.style.width='42px';
-    img.style.height='57px';
+function pcSelection() {
+  pcChoice = getRandomNumber();
+  let img = document.querySelector(".hand-sign-2 img");
+  img.src = `${url[pcChoice]}`;
+  if (pcChoice == 0) {
+    img.style.width = "42px";
+    img.style.height = "57px";
   }
-  if(imgChoice==1){
-    img.style.width='72px';
-    img.style.height='72px';
+  if (pcChoice == 1) {
+    img.style.width = "72px";
+    img.style.height = "72px";
   }
-  if(imgChoice==2){
-    img.style.width='80px';
-    img.style.height='59px';
+  if (pcChoice == 2) {
+    img.style.width = "80px";
+    img.style.height = "59px";
   }
-
 }
 
-function getRandomNumber(){
-  let randomNumber=Math.ceil(Math.random()*3);
-  return randomNumber-1;
+function getRandomNumber() {
+  let randomNumber = Math.ceil(Math.random() * 3);
+  return randomNumber - 1;
+}
+
+function removeFirstScreen() {
+  let element = document.querySelectorAll(".game");
+  let halo = document.querySelectorAll(".halo");
+
+  halo.forEach((element) => element.remove());
+  element.forEach((element) => element.remove());
+
+  addSecondScreen();
+}
+
+function addSecondScreen() {
+  const container = document.createElement("div");
+  winner = decideWinner();
+  if (winner == "you") {
+    container.innerHTML = `
+  <div class="game-container">
+    <div class="left-handsign-container">
+    <div class="halo">
+      <div class="circle1"></div>
+      <div class="circle2"></div>
+      <div class="circle3"></div>
+</div> 
+      <div class="you-picked">YOU PICKED</div>
+      <div class="hand-sign ${yourChoiceStyle[yourChoice]}" id="left">
+          <img src="${url[yourChoice]}" />
+        </div>
+    </div>
+    <div class="right-handsign-container">
+             <div class="hand-sign ${yourChoiceStyle[pcChoice]}" id="right">
+          <img src="${url[pcChoice]}"  />
+        </div>
+    </div>
+  </div>`;
+  }
+  if (winner == "pc") {
+    container.innerHTML = `
+  <div class="game-container">
+    <div class="left-handsign-container">
+    
+      <div class="you-picked">YOU PICKED</div>
+      <div class="hand-sign ${yourChoiceStyle[yourChoice]}" id="left">
+          <img src="${url[yourChoice]}" />
+        </div>
+    </div>
+    <div class="right-handsign-container">
+            <div class="halo">
+              <div class="circle1"></div>
+              <div class="circle2"></div>
+              <div class="circle3"></div>
+            </div> 
+             <div class="hand-sign ${yourChoiceStyle[pcChoice]}" id="right">
+          <img src="${url[pcChoice]}"  />
+        </div>
+    </div>
+  </div>`;
+  }
+
+  if (winner == "draw") {
+    container.innerHTML = `
+  <div class="game-container">
+    <div class="left-handsign-container">
+    
+      <div class="you-picked">YOU PICKED</div>
+      <div class="hand-sign ${yourChoiceStyle[yourChoice]}" id="left">
+          <img src="${url[yourChoice]}" />
+        </div>
+    </div>
+    <div class="right-handsign-container">
+          <div class="hand-sign ${yourChoiceStyle[pcChoice]}" id="right">
+          <img src="${url[pcChoice]}"  />
+          </div>
+    </div>
+  </div>`;
+  }
+
+  document.body.appendChild(container);
+
+  const linkElement = document.querySelector(
+    'link[rel="stylesheet"][href="style.css"]'
+  );
+
+  // Change the href attribute to point to style2.css
+  if (linkElement) {
+    linkElement.href = "style2.css";
+  }
+
+
+    document.getElementById("left").style.left = "150px";
+    document.getElementById("left").style.top = "200px";
+    document.getElementById("right").style.right = "100px";
+    document.getElementById("right").style.top = "200px";
+
+    if(winner=="pc"){
+      document.querySelector('.halo').style.left='115px';
+    }
+
+
+  startHaloAnimation();
+}
+
+function startHaloAnimation() {
+  if (haloInterval) clearInterval(haloInterval);
+
+  var haloInterval = setInterval(() => {
+    haloAnimation();
+  }, 1800);
+
+  haloAnimation();
+}
+
+function haloAnimation() {
+  let circles = document.querySelectorAll(".circle1, .circle2, .circle3");
+  let reversedCircles = Array.from(circles).reverse();
+  reversedCircles.forEach((circle, index) => {
+    setTimeout(() => {
+      circle.style.opacity = 1;
+    }, index * 200 + 400);
+
+    setTimeout(() => {
+      circle.style.opacity = 0;
+    }, index * 400 + 1200);
+  });
+}
+
+function stopHaloAnimation() {
+  clearInterval(haloInterval);
+  haloInterval = null;
+}
+
+function decideWinner() {
+  if (yourChoice == 0) {
+    if (pcChoice == 1) {
+      winner = "you";
+    } else winner = "pc";
+  }
+  if (yourChoice == 1) {
+    if (pcChoice == 2) {
+      winner = "you";
+    } else winner = "pc";
+  }
+  if (yourChoice == 2) {
+    if (pcChoice == 0) {
+      winner = "you";
+    } else winner = "pc";
+  }
+  if (yourChoice == pcChoice) {
+    winner = "draw";
+  }
+  return winner;
 }
